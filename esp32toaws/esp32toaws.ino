9377d177ba1850016a02f8cb539bdb94c53576cd
LinusWase <linusmwase@gmail.com>
2024-11-28 12:36:29 +0100

    First commit

---
+++ esp32toaws/esp32toaws.ino
@@ -0,0 +1,17 @@
+//Include necessary libraries
+
+#include "secrets.h"
+#include <WiFiClientSecure.h>
+#include <MQTTClient.h> //MQTT Library Source: https://github.com/256dpi/arduino-mqtt
+#include <ArduinoJson.h> //ArduinoJson Library Source: https://github.com/bblanchon/ArduinoJson
+#include "WiFi.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ esp32toaws/secrets.h
@@ -0,0 +1,27 @@
+#include <pgmspace.h>
+const char* ssid ="<Your SSID>";
+const char* password="Your Password";
+const char* AWS_IOT_ENDPOINT="xxxxxxxx.amazons.com";
+
+//Amazon Root CA 1 
+static const char AWS_CERT_CA[] PROGMEM =R"EOF(
+  -----BEGIN CERTIFICATE-----
+
+  -----END CERTIFICATE-----
+)EOF";
+
+
+//Device Certificate
+static const char AWS_CERT_CRT[] PROGMEM =R"KEY(
+  -----BEGIN CERTIFICATE-----
+
+  -----END CERTIFICATE-----
+)KEY";
+
+
+//Deivce Private Key 
+static const char AWS_CERT_PRIVATE[] PROGMEM =R"KEY(
+  -----BEGIN RSA PRIVATE KEY-----
+
+  -----END RSA PRIVATE KEY -----
+)KEY";
